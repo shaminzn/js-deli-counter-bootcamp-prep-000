ae7e7772c37312a46b419247866b0be02af5fa6b
Shamira Naznin <shaminzn@gmail.com>
2020-03-14 20:26:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 function takeANumber(peopleInLine, newName){
-  lineOfPeople.push(`${newName}`);
-  return(`Welcome, ${newName}. You are number ${lineOfPeople.length} in line.`);
+  peopleInLine.push(`${newName}`);
+  return(`Welcome, ${newName}. You are number ${peopleInLine.length} in line.`);
 }
 
-function nowServing(lineOfPeople){
+function nowServing(peopleInLine){
   let i = 0;
-  while (i < lineOfPeople.length){
+  while (i < peopleInLine.length){
     i++;
   }
-  if (lineOfPeople.length === 0){
+  if (peopleInLine.length === 0){
     return "There is nobody waiting to be served!"
   }else {
-    return `Currently serving ${lineOfPeople.shift()}.`;
+    return `Currently serving ${peopleInLine.shift()}.`;
   }
 }
 
 var line = [];
 
-function currentLine(lineOfPeople){
+function currentLine(peopleInLine){
   let i = 0;
-  while (i < lineOfPeople.length){
-    line.push(` ` + [i + 1] + `. ` + lineOfPeople[i])
+  while (i < peopleInLine.length){
+    line.push(` ` + [i + 1] + `. ` + peopleInLine[i])
     i++;
   }
-  if (lineOfPeople.length === 0){
+  if (peopleInLine.length === 0){
     return "The line is currently empty."
   }else{
     return "The line is currently:" + line
